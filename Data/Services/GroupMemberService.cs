03c6d371a19bae0f4e14c14c27aa815b244d3d97
piyachai kitkan <oonetino2543@gmail.com>
2023-01-25 16:33:07 +0700

    เสร็จ

--- Data/Services/GroupMemberService.cs
+++ Data/Services/GroupMemberService.cs
@@ -15,12 +15,6 @@ namespace CW_ordermedicine.Data.Services
 		}
 		//private List<GroupMember> Groups = new List<GroupMember>()
 		//{
-		//	 new GroupMember(){GroupID=0, GroupName="คนที่ไม่มีกลุ่ม",  },
-		//		new GroupMember(){GroupID=1, GroupName="A Group",   },
-		//		new GroupMember(){GroupID=2, GroupName="B Group", },
-		//		new GroupMember(){GroupID=3, GroupName="C Group",  },
-		//		new GroupMember(){GroupID=4, GroupName="D Group",  },
-
 		//};
 		//public async Task<List<GroupMember>> GetGroupAsync()
 		//{
@@ -48,21 +42,19 @@ namespace CW_ordermedicine.Data.Services
 			_Context.TableGroupMember.Remove(FindID);
 			_Context.SaveChanges();
 		}
-
-		//public void SelectGroup()
-		//{
-		//	var u = _Context.TableGroupMember.Count();
-
-		//}
 		public void SelectGroup2(TableGroupMember b)
 		{
 			var FindID = _Context.TableGroupMember.First(e => e.GroupID == b.GroupID);
 
 		}
-		//public void getdata()
-		//{
-		//	var u = _Context.TableGroupMember.ToList();
+		public void EditGroup(TableGroupMember EditGroup)
+		{
+			var FindID = _Context.TableGroupMember.First(e => e.GroupID == EditGroup.GroupID);
+			FindID.GroupName = EditGroup.GroupName;
+			FindID.Department = EditGroup.Department;
 
-		//}
+			_Context.TableGroupMember.Update(FindID);
+			_Context.SaveChanges();//บันทึกลง SQL
+		}
 	}
 }
